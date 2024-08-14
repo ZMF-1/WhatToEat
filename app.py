bd77eafd7f57687fa7d870ceb301c9ae472cf60e
ZMF-1 <97657213+ZMF-1@users.noreply.github.com>
2024-08-14 11:00:15 +0800

    Add SSL Config.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sslify import SSLify
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='')
+sslify = SSLify(app)
+
+# If you have SSL
+@app.before_request
+def force_https():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 
 @app.route('/')
