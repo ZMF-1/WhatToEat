4d1aafcb1558405ef244bc9dfa24198d5a643a5d
ZMF-1 <97657213+ZMF-1@users.noreply.github.com>
2024-08-18 19:40:31 +0800

    Version 1.0.0

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ def hello_world():  # put application's code here
     if os.path.exists("days.wte") and os.path.isfile("days.wte"):
         with open("days.wte", 'r+', encoding='utf-8') as f:
             liveddates = f.readlines()
+            for i in range(len(liveddates)):
+                liveddates[i] = liveddates[i].rstrip('\n')
         td = str(datetime.date.today())
         if td in liveddates:
             pass
@@ -29,12 +31,12 @@ def hello_world():  # put application's code here
             s = datetime.date.today() - datetime.timedelta(datetime.date.weekday(datetime.date.today()))
             for i in [0, 1, 2, 3, 4, 5, 6]:
                 with open("days.wte", 'a+', encoding='utf-8') as f2:
-                    f2.write(str(s + datetime.timedelta(days=i)))
+                    f2.write(str(s + datetime.timedelta(days=i)) + "\n")
             with open("menu.lst", "w+", encoding="utf-8") as f:
                 menu = []
                 meat = ['照烧鸡排', '照烧猪颈肉', '白灼虾', '红烧肉/排骨', '手撕鸡', '煎三文鱼', '炸鸡块', '炸鳕鱼块', '滑蛋牛肉', '清蒸鲈鱼', '蘑菇炖鸡', '滑蛋虾仁', '烤羊排', '洋葱炒猪肝', '蟹柳虾仁炒蛋', '咖喱鸡', '青椒炒肉丝', '夫妻肺片']
                 vegt = ['生菜', '菠菜', '西兰花', '娃娃菜', '荷兰豆', '秋葵', '丝瓜炒蛋', '番茄炒蛋', '香菇炒青菜', '炒木耳菜', '竹笋香菇面筋', '黄瓜木耳炒蛋', '韭黄炒蛋', '肉末茄子', '青椒炒蛋', '芦笋炒蛋', '炒空心菜', '芹菜鸡蛋干', '拍黄瓜']
-                soup = ['丝瓜蛋汤', '番茄蛋汤', '紫菜蛋汤', '娃娃菜一锅炖', '蒸蛋', '牛肉豆腐锅', '竹荪鸡汤', '萝卜香菇排骨汤', '玉米山药排骨汤', '皮蛋豆腐', '素鸡', '腐皮卷', '卤鹌鹑蛋', '土豆泥色拉', '/', '/', '/', '/', '/', '/', '/']
+                soup = ['丝瓜蛋汤', '番茄蛋汤', '紫菜蛋汤', '娃娃菜一锅炖', '蒸蛋', '牛肉豆腐锅', '竹荪鸡汤', '萝卜香菇排骨汤', '玉米山药排骨汤', '皮蛋豆腐', '素鸡', '腐皮卷', '卤鹌鹑蛋', '土豆泥色拉', '/', '/', '/', '/', '/']
                 rice = ['鲜虾鸡蛋面', '红烧鸡腿面', '番茄鸡蛋面', '猪排饭', '亲子饭', '蛋包饭', '菜饭', '三文鱼意面', '鸡胸肉意面', '小馄饨', '饺子', '生菜牛肉拌饭', '三文鱼菌菇饭', '烤肠芦笋意面', '牛排意面', '塔塔配烤肠', '虾仁鸡肉碗']
                 metu = ['照烧鸡排饭-食其家', '汉堡王套餐', '麦当劳套餐', '振鼎鸡拌面套餐', '小杨生煎套餐', '寿司-酯士道/雪盈', '猪排饭-花火', '叉烧饭套餐-富荣祥', '拌将麻辣烫', '大米先生套餐']
                 random.shuffle(meat)
@@ -43,7 +45,7 @@ def hello_world():  # put application's code here
                 random.shuffle(rice)
                 random.shuffle(metu)
                 for i in range(7):
-                    if random.randint(0, 1) == 0:
+                    if random.randint(0, 100) < 98:
                         d = [meat[i], vegt[i], soup[i], meat[-i], vegt[-i], metu[i]]
                         menu.append(d)
                     else:
@@ -54,7 +56,7 @@ def hello_world():  # put application's code here
         s = datetime.date.today() - datetime.timedelta(datetime.date.weekday(datetime.date.today()))
         for i in [0, 1, 2, 3, 4, 5, 6]:
             with open("days.wte", 'a+', encoding='utf-8') as f2:
-                f2.write(str(s + datetime.timedelta(days=i)))
+                f2.write(str(s + datetime.timedelta(days=i)) + "\n")
         with open("menu.lst", "w+", encoding="utf-8") as f:
             menu = []
             meat = ['照烧鸡排', '照烧猪颈肉', '白灼虾', '红烧肉/排骨', '手撕鸡', '煎三文鱼', '炸鸡块', '炸鳕鱼块',
@@ -102,7 +104,22 @@ def daily():
     <td>外卖</td>
   </tr>
     """
-    for i in range(7):
+    for j in range(1):
+        i = datetime.date.weekday(datetime.date.today())
+        if i == 0:
+            wd = "星期一"
+        elif i == 1:
+            wd = "星期二"
+        elif i == 2:
+            wd = "星期三"
+        elif i == 3:
+            wd = "星期四"
+        elif i == 4:
+            wd = "星期五"
+        elif i == 5:
+            wd = "星期六"
+        elif i == 6:
+            wd = "星期日"
         tb += """  <tr>
     <td>{}</td>
     <td>{}</td>
@@ -112,26 +129,51 @@ def daily():
     <td>{}</td>
     <td>{}</td>
   </tr>
-        """.format(menu[i][0], menu[i][1], menu[i][2], menu[i][3], menu[i][4], menu[i][5], menu[i][6])
-        tb += "</table>"
+        """.format(wd, menu[i][0], menu[i][1], menu[i][2], menu[i][3], menu[i][4], menu[i][5])
+    tb += "</table>"
     return render_template("show.html", food_lst=tb)
 
 @app.route('/weekly')
 def weekly():
+    with open("menu.lst", 'r+', encoding='utf-8') as f:
+        menu = eval(f.read())
     tb = """<table border="1">
-<tr>
-  <td>100</td>
-  <td>200</td>
-  <td>300</td>
-</tr>
-<tr>
-  <td>400</td>
-  <td>500</td>
-  <td>600</td>
-</tr>
-</table>
-    """
-    tb = "???"
+      <tr>
+        <td>日期</td>
+        <td>荤菜</td>
+        <td>素菜</td>
+        <td>汤/凉菜</td>
+        <td>备选荤</td>
+        <td>备选素</td>
+        <td>外卖</td>
+      </tr>
+        """
+    for i in range(7):
+        if i == 0:
+            wd = "星期一"
+        elif i == 1:
+            wd = "星期二"
+        elif i == 2:
+            wd = "星期三"
+        elif i == 3:
+            wd = "星期四"
+        elif i == 4:
+            wd = "星期五"
+        elif i == 5:
+            wd = "星期六"
+        elif i == 6:
+            wd = "星期日"
+        tb += """  <tr>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+      </tr>
+            """.format(wd, menu[i][0], menu[i][1], menu[i][2], menu[i][3], menu[i][4], menu[i][5])
+    tb += "</table>"
     return render_template("show.html", food_lst=tb)
 
 
